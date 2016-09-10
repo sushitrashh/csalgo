b0efae68e19ad156209279cd8f958d5031885f8b
AARON EZRA LUCILA <AALUCILA@fit.edu.ph>
2016-09-10 20:46:13 +0800

    TEST STRING

--- LexicographicGenerator/src/lexicographicgenerator/LexicographicGenerator.java
+++ LexicographicGenerator/src/lexicographicgenerator/LexicographicGenerator.java
@@ -13,7 +13,10 @@ public class LexicographicGenerator {
 
 
     public static void main(String[] args) {
- 
+            String charset = "ABC";
+            for (int i=0;i<charset.length();i++)
+                System.out.println(charset.charAt(i));
+            System.out.println("pakshet");
     }
     
 }
